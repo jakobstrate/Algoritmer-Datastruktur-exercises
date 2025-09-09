8db04fa6207dca6970f2fc1907cbea86af4e2a59
Jakob Strate <jastr23@student.sdu.com>
2025-09-09 15:01:58 +0200

    made first two exercises

--- src/main/java/dk/cbse/jakob/Exercise1.java
+++ src/main/java/dk/cbse/jakob/Exercise1.java
@@ -1,4 +1,15 @@
 package dk.cbse.jakob;
 
+
 public class Exercise1 {
+    public static int logTwo(int N){
+        if (N==1){
+            return 0;
+        }
+        return 1 + logTwo(N / 2);
+    }
+    public static void main(String[] args) {
+        System.out.println(logTwo(32));
+        System.out.println(logTwo(4096));
+    }
 }
---
+++ src/main/java/dk/cbse/jakob/Exercise2.java
@@ -0,0 +1,18 @@
+package dk.cbse.jakob;
+
+public class Exercise2 {
+    public static int countChars(String str, char c) {
+        if (str.isEmpty()) {
+            return 0;
+        }
+
+        int count = (str.charAt(0) == c) ? 1 : 0;
+        return (count + countChars(str.substring(1), c));
+    }
+
+    public static void main(String[] args) {
+        String word = "banana";
+        char targetChar = 'a';
+        System.out.println(countChars(word, targetChar));
+    }
+}
